34fa53c085a7fac8c1370cd29565603181b60619
Marko Milić <marko@tesobe.com>
2024-10-16 10:47:42 +0200

    refactor/Get rid of macmemo 0.6-OBP-SNAPSHOT library

--- obp-api/src/main/scala/code/api/cache/Caching.scala
+++ obp-api/src/main/scala/code/api/cache/Caching.scala
@@ -3,14 +3,11 @@ package code.api.cache
 import code.api.Constant._
 import code.api.JedisMethod
 import code.api.cache.Redis.use
-import code.api.util.APIUtil
 import code.util.Helper.MdcLoggable
-import com.softwaremill.macmemo.{Cache, MemoCacheBuilder, MemoizeParams}
 
 import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 import scala.language.postfixOps
-import scala.reflect.runtime.universe._
 object Caching extends MdcLoggable {
 
   def memoizeSyncWithProvider[A](cacheKey: Option[String])(ttl: Duration)(f: => A)(implicit m: Manifest[A]): A = {
@@ -35,44 +32,6 @@ object Caching extends MdcLoggable {
         f
     }
 
-  }
-
-  /**
-   * the default MemoCacheBuilder for annotation OBPMemoize
-   *
-   * e.g:
-   *{{{
-   *   import Caching._
-   *
-   *     @OBPMemoize(ttl = 2 hours, maxSize = 111)
-   *     def hello(name: String, age: Int): Future[String] = ???
-   *}}}
-   */
-  implicit object OBPCacheBuilder extends  MemoCacheBuilder {
-    override def build[V : TypeTag : Manifest](bucketId: String, params: MemoizeParams): Cache[V] = new Cache[V] {
-      val ttl = params.expiresAfterMillis
-      var isFuture = implicitly[TypeTag[V]].tpe <:< typeOf[Future[_]]
-      var fixedReturnType = false
-
-      override def get(key: List[Any], compute: => V): V = {
-        val cacheKey = bucketId + "_" + key.mkString("_")
-        if(isFuture) {
-          val result = memoizeWithProvider(Some(cacheKey))(ttl)(compute.asInstanceOf[Future[Any]])
-          result.asInstanceOf[V]
-        } else if(implicitly[TypeTag[V]].tpe =:= typeOf[Any] && !fixedReturnType) {
-          val result = compute
-          isFuture = result.isInstanceOf[Future[_]]
-          fixedReturnType = true
-          this.get(key, result)
-        } else {
-          val result = memoizeSyncWithProvider(Some(cacheKey))(ttl)(compute)
-          if(result.isInstanceOf[Future[_]]) {
-            isFuture = true
-          }
-          result
-        }
-      }
-    }
   }
 
   def getLocalisedResourceDocCache(key: String) = {

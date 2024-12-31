7fd2031942bad89320b5933a0b3c033ae8c082d0
Marko Milić <marko@tesobe.com>
2024-12-31 21:03:20 +0100

    bugfix/Fix end month/year range bug at endpoint getCounterpartyLimitStatus

--- obp-api/src/main/scala/code/api/v5_1_0/APIMethods510.scala
+++ obp-api/src/main/scala/code/api/v5_1_0/APIMethods510.scala
@@ -3601,10 +3601,22 @@ trait APIMethods510 {
             // Convert LocalDate to Date
             zoneId: ZoneId = ZoneId.systemDefault()
             firstCurrentMonthDate: Date = Date.from(firstDayOfMonth.atStartOfDay(zoneId).toInstant)
-            lastCurrentMonthDate: Date = Date.from(lastDayOfMonth.atStartOfDay(zoneId).toInstant)
+            // Adjust to include 23:59:59.999
+            lastCurrentMonthDate: Date = Date.from(
+              lastDayOfMonth
+                .atTime(23, 59, 59, 999000000)
+                .atZone(zoneId)
+                .toInstant
+            )
 
             firstCurrentYearDate: Date = Date.from(firstDayOfYear.atStartOfDay(zoneId).toInstant)
-            lastCurrentYearDate: Date = Date.from(lastDayOfYear.atStartOfDay(zoneId).toInstant)
+            // Adjust to include 23:59:59.999
+            lastCurrentYearDate: Date = Date.from(
+              lastDayOfYear
+                .atTime(23, 59, 59, 999000000)
+                .atZone(zoneId)
+                .toInstant
+            )
 
             defaultFromDate: Date = theEpochTime
             defaultToDate: Date = APIUtil.ToDateInFuture

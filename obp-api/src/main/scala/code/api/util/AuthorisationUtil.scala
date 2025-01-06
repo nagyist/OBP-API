176bd92423f2c36817afd5eb669bce584eb4bf86
Marko Milić <marko@tesobe.com>
2025-01-06 14:54:37 +0100

    feature/Check Auth headers ambiguity 3

--- obp-api/src/main/scala/code/api/util/AuthorisationUtil.scala
+++ obp-api/src/main/scala/code/api/util/AuthorisationUtil.scala
@@ -7,9 +7,6 @@ object AuthorisationUtil {
   def getAuthorisationHeaders(requestHeaders: List[HTTPParam]): List[String] = {
     requestHeaders.map(_.name).filter {
       case `Consent-Id`| `Consent-ID` | `Consent-JWT` => true
-      case "Authorization" => true
-      case "DirectLogin" => true
-      case "GatewayLogin" => true
       case _ => false
     }
   }
